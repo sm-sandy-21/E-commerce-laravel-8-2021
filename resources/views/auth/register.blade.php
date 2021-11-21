b520843e251e67e20ccfd6b45b034b5c3e5eb130
sandy <mdsandy21@gmail.com>
2021-11-21 19:21:17 +0600

    Changing the Login and Register Page Layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,77 @@
 <x-guest-layout>
+    <main id="main" class="main-site left-sidebar">
+		<div class="container">
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="#" class="link">home</a></li>
+					<li class="item-link"><span>Register</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">							
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">
+							<div class="register-form form-item ">
+
+                                <x-jet-validation-errors class="mb-4" />
+
+								<form method="POST" action="{{ route('register') }}" class="form-stl" name="frm-login">
+                                    @csrf
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Create an account</h3>
+										<h4 class="form-subtitle">Personal infomation</h4>
+									</fieldset>									
+									<fieldset class="wrap-input">
+										<label for="frm-reg-lname">Name*</label>
+										<input type="text" id="frm-reg-lname" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Last name*">
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-reg-email">Email Address*</label>
+										<input type="email" id="frm-reg-email" name="email" :value="old('email')" required placeholder="Email address">
+									</fieldset>
+									{{-- <fieldset class="wrap-functions ">
+										<label class="remember-field">
+											<input name="newletter" id="new-letter" value="forever" type="checkbox"><span>Sign Up for Newsletter</span>
+										</label>
+									</fieldset> --}}
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Login Information</h3>
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half left-item ">
+										<label for="frm-reg-pass">Password *</label>
+										<input type="text" id="frm-reg-pass" placeholder="Password" name="password" required autocomplete="new-password">
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half ">
+										<label for="frm-reg-cfpass">Confirm Password *</label>
+										<input type="text" id="frm-reg-cfpass" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+
+            
+									</fieldset>
+									<input type="submit" class="btn btn-sign" value="Register" name="register">
+                                    <a class="link-function left-position" href="{{ route('login') }}" title="Forgotten password?">Already have an account?</a>
+								</form>
+							</div>											
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main>
+</x-guest-layout>
+
+
+
+
+
+
+
+
+
+
+
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -57,4 +130,4 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -37,7 +37,7 @@
 						<div class="topbar-menu left-menu">
 							<ul>
 								<li class="menu-item" >
-									<a title="Hotline: (+123) 456 789" href="#" ><span class="icon label-before fa fa-mobile"></span>Hotline: (+880) 0179279</a>
+									<a title="Hotline: (+880) 0179279" href="#" ><span class="icon label-before fa fa-mobile"></span>Hotline: (+880) 0179279</a>
 								</li>
 							</ul>
 						</div>
@@ -45,12 +45,12 @@
 							<ul>
 								
 								<li class="menu-item lang-menu menu-item-has-children parent">
-									<a title="English" href="#"><span class="img label-before"><img src="assets/images/lang-en.png" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									<a title="English" href="#"><span class="img label-before"><img src={{asset("assets/images/lang-en.png")}} alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
-										<li class="menu-item" ><a title="hungary" href="#"><span class="img label-before"><img src="assets/images/lang-hun.png" alt="lang-hun"></span>Hungary</a></li>
-										<li class="menu-item" ><a title="german" href="#"><span class="img label-before"><img src="assets/images/lang-ger.png" alt="lang-ger" ></span>German</a></li>
-										<li class="menu-item" ><a title="french" href="#"><span class="img label-before"><img src="assets/images/lang-fra.png" alt="lang-fre"></span>French</a></li>
-										<li class="menu-item" ><a title="canada" href="#"><span class="img label-before"><img src="assets/images/lang-can.png" alt="lang-can"></span>Canada</a></li>
+										<li class="menu-item" ><a title="hungary" href="#"><span class="img label-before"><img src={{asset("assets/images/lang-hun.png")}} alt="lang-hun"></span>Hungary</a></li>
+										<li class="menu-item" ><a title="german" href="#"><span class="img label-before"><img src={{asset("assets/images/lang-ger.png")}} alt="lang-ger" ></span>German</a></li>
+										<li class="menu-item" ><a title="french" href="#"><span class="img label-before"><img src={{asset("assets/images/lang-fra.png")}} alt="lang-fre"></span>French</a></li>
+										<li class="menu-item" ><a title="canada" href="#"><span class="img label-before"><img src={{asset("assets/images/lang-can.png")}} alt="lang-can"></span>Canada</a></li>
 									</ul>
 								</li>
 								<li class="menu-item menu-item-has-children parent" >
